14d380312aeabc5099e7f7e8ada20afa5267534b
Baboo7 <baptiste.studer@laposte.net>
2017-09-17 12:15:00 -0700

    handle student and teacher connection and disconnection

--- websocket.js
+++ websocket.js
@@ -1,19 +1,38 @@
 const uuidv4 = require('uuid/v4');
 
-let clients = {
-  students: {},
-  teachers: {}
+/*
+  students and teachers have the following shape for a given id:
+    · emitterType (string)
+    · socket (socket object)
+*/
+let sockets = {
+  students: { },
+  teachers: { }
 };
 
 const setUpWebsocket = server => {
   let io = require('socket.io')(server);
 
   io.on('connection', socket => {
-    console.log(`new user connected`);
+    let id = uuidv4();
 
-    socket.on('get-uuid', data => {
-      let id = uuidv4();
-      socket.emit('set-uuid', {id: id});
+    socket.on('init', data => {
+      let emitterType = data.emitterType;
+      console.log(`new ${emitterType} connected ${id}`);
+
+      if (emitterType === 'student') {
+        sockets.students[id] = { emitterType, socket };
+        Object.keys(sockets.teachers).forEach(teacherId => {
+          sockets.teachers[teacherId].socket.emit('new-students', { students: [ id ] });
+        });
+      } else if (emitterType === 'teacher') {
+        sockets.teachers[id] = { emitterType, socket };
+        socket.emit('new-students', { students: Object.keys(sockets.students) });
+      } else {
+        console.error('unexpected emitter type', emitterType, socket);
+      }
+
+      socket.emit('set-id', {id: id});
     });
 
     socket.on('message', data => {
@@ -23,6 +42,14 @@ const setUpWebsocket = server => {
 
     socket.on('disconnect', () => {
       console.log(`user disconnected`);
+      if (sockets.students[id] !== undefined) {
+        delete sockets.students[id];
+        Object.keys(sockets.teachers).forEach(teacherId => {
+          sockets.teachers[teacherId].socket.emit('del-student', { student: [ id ] });
+        });
+      } else if (sockets.teachers[id] !== undefined) {
+        delete sockets.teachers[id];
+      }
     });
   });
 }
