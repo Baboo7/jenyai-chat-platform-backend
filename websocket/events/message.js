0c944ff25e1fba2009c2c1f9fff320499b175f9a
Baboo7 <baptiste.studer@laposte.net>
2017-09-23 19:27:03 -0700

    reflect message sent

--- websocket/events/message.js
+++ websocket/events/message.js
@@ -9,28 +9,33 @@ const init = (data, user) => {
   console.log(`message received from ${user.type} ${user.userId}: `, data);
 
   if (user.type === 'student') {
+    let message = {
+      emitter: user.userId,
+      emitterType: user.type,
+      message: {
+        type: 'text',
+        payload: data.payload
+      }
+    };
+    sockets[user.roomId][user.type][user.userId].socket.emit('message', message);
     Object.keys(sockets[user.roomId]['teacher']).forEach(id => {
-      sockets[user.roomId]['teacher'][id].socket.emit('message',
-      {
+      sockets[user.roomId]['teacher'][id].socket.emit('message', message);
+    });
+  } else if (user.type === 'teacher') {
+    if (data.recipient) {
+      let message = {
         emitter: user.userId,
+        emitterType: user.type,
+        recipient: data.recipient,
         message: {
           type: 'text',
           payload: data.payload
         }
-      });
-    });
-  } else if (user.type === 'teacher') {
-    if (data.recipient) {
+      };
       let student = sockets[user.roomId]['student'][data.recipient];
       if (student !== undefined) {
-        student.socket.emit('message',
-        {
-          emitter: user.userId,
-          message: {
-            type: 'text',
-            payload: data.payload
-          }
-        });
+        student.socket.emit('message', message);
+        sockets[user.roomId][user.type][user.userId].socket.emit('message', message);
       }
     }
   }
