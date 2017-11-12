091d24a462f4aaaecffaf740bbff99fceeda1642
Baboo7 <baptiste.studer@laposte.net>
2017-11-12 10:57:58 -0800

    update message event doc

--- websocket/events/message.js
+++ websocket/events/message.js
@@ -4,11 +4,21 @@ const messenger = require('../../messenger');
 const userManager = require('../managers/user');
 let sockets = require('../sockets');
 
+/*  Message types:
+
+    > 'text'
+
+    payload (object)
+      text (string)
+      media (string): the way the message has been entered
+*/
+
 /*  Transmits a message from one client to an other.
 
     PARAMS
       data (object): object sent by the client. It must contain
-        payload (string): text message sent by the user
+        type (string): message type
+        payload (object): information contained depends on message type (see above)
       socketId (string): socket id
 
     RETURN
